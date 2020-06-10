f60f1532647c7f13751f687a1fadb87f13bc4e6c
tyatoshi <ok.818twin5bk.cpa@gmail.com>
2020-06-10 17:14:12 +0900

    プロジェクト ファイルを追加します。

---
+++ sample4/Controllers/TopController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sample4.Controllers
+{
+    public class TopController : Controller
+    {
+        // GET: top
+        public ActionResult index()
+        {
+            return View();
+        }
+    }
+}
---
+++ sample4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="sample4.MvcApplication" Language="C#" %>
